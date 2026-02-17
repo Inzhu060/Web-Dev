679ce98607d94de1dccfefa03ae414ef1e4ccaac
Inzhu060 <inzhunurbergenova@gmail.com>
2026-02-17 12:26:14 +0500

    Lab4: task1 Formatting data with pipes

--- Lab4/task1/src/app/app.ts
+++ Lab4/task1/src/app/app.ts
@@ -1,11 +1,19 @@
 import {Component} from '@angular/core';
-import {LowerCasePipe} from '@angular/common';
+import {DecimalPipe, DatePipe, CurrencyPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  template: ` {{ username | lowercase }} `,
-  imports: [LowerCasePipe],
+  template: `
+    <ul>
+      <li>Number with "decimal" {{ num | number: '3.2-2' }}</li>
+      <li>Date with "date" {{ birthday | date: 'medium' }}</li>
+      <li>Currency with "currency" {{ cost | currency }}</li>
+    </ul>
+  `,
+  imports: [DecimalPipe, DatePipe, CurrencyPipe],
 })
 export class App {
-  username = 'yOunGTECh';
+  num = 103.1234;
+  birthday = new Date(2023, 3, 2);
+  cost = 4560.34;
 }
