5bf7c207ea4e267fc64f27b534af6e6abfc28660
Inzhu060 <inzhunurbergenova@gmail.com>
2026-02-17 11:20:22 +0500

    Lab4: task1 Optimizing Images

--- Lab4/task1/src/app/user.ts
+++ Lab4/task1/src/app/user.ts
@@ -1,9 +1,26 @@
-import {Component, input} from '@angular/core';
+import {Component} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-user',
-  template: ` <p>The user's name is {{ name() }}</p> `,
+  template: `
+    <p>Username: {{ username }}</p>
+    <p>Preferred Framework:</p>
+    <ul>
+      <li>
+        Static Image:
+        <img ngSrc="/logo.svg" alt="Angular logo" width="32" height="32" />
+      </li>
+      <li>
+        Dynamic Image:
+        <img [ngSrc]="logoUrl" [alt]="logoAlt" width="32" height="32" />
+      </li>
+    </ul>
+  `,
+  imports: [NgOptimizedImage],
 })
 export class User {
-  readonly name = input<string>();
+  logoUrl = '/logo.svg';
+  logoAlt = 'Angular logo';
+  username = 'youngTech';
 }
