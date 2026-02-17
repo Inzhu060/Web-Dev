2ce9fdd3a40353744b80958f3b3f20632f2545fc
Inzhu060 <inzhunurbergenova@gmail.com>
2026-02-17 11:34:16 +0500

    Lab4: task1 Getting form control value

--- Lab4/task1/src/app/user.ts
+++ Lab4/task1/src/app/user.ts
@@ -5,15 +5,20 @@ import {FormsModule} from '@angular/forms';
   selector: 'app-user',
   template: `
     <p>Username: {{ username }}</p>
-    <p>{{ username }}'s favorite framework: {{ favoriteFramework }}</p>
+    <p>Framework: {{ favoriteFramework }}</p>
     <label for="framework">
       Favorite Framework:
       <input id="framework" type="text" [(ngModel)]="favoriteFramework" />
     </label>
+    <button (click)="showFramework()">Show Framework</button>
   `,
   imports: [FormsModule],
 })
 export class User {
   favoriteFramework = '';
   username = 'youngTech';
+
+  showFramework() {
+    alert(this.favoriteFramework);
+  }
 }
