48754c935f51625e8a23c2a5ca70aed9faef8f70
Inzhu060 <inzhunurbergenova@gmail.com>
2026-02-17 11:57:32 +0500

    Lab4: task1 Creating an injectable service

--- Lab4/task1/src/app/app.ts
+++ Lab4/task1/src/app/app.ts
@@ -1,21 +1,11 @@
-import {Component} from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
-import {ReactiveFormsModule} from '@angular/forms';
+import {Component, inject} from '@angular/core';
+import {CarService} from './car.service';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <form [formGroup]="profileForm">
-      <input type="text" formControlName="name" name="name" />
-      <input type="email" formControlName="email" name="email" />
-      <button type="submit">Submit</button>
-    </form>
-  `,
-  imports: [ReactiveFormsModule],
+  template: '<p> {{ carService.getCars() }} </p>',
 })
 export class App {
-  profileForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-  });
+  carService = inject(CarService);
 }
+
