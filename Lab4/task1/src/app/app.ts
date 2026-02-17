95ddbf4193f57b1c4649f9f97ea8e3496b916b4a
Inzhu060 <inzhunurbergenova@gmail.com>
2026-02-17 11:10:00 +0500

    Lab4: task1 Component Input

--- Lab4/task1/src/app/app.ts
+++ Lab4/task1/src/app/app.ts
@@ -1,18 +1,9 @@
 import {Component} from '@angular/core';
+import {User} from './user';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <section (mouseover)="showSecretMessage()">
-      There's a secret message for you, hover to reveal 👀
-      {{ message }}
-    </section>
-  `,
+  template: ` <app-user name="Simran" /> `,
+  imports: [User],
 })
-export class App {
-  message = '';
-
-  showSecretMessage() {
-    this.message = 'Way to go 🚀';
-  }
-}
+export class App {}
