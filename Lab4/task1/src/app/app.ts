7fc751ba238b66c8f24721fbc995503f2d7529ea
Inzhu060 <inzhunurbergenova@gmail.com>
2026-02-17 10:43:41 +0500

    Lab4: task1 control flow-> for

--- Lab4/task1/src/app/app.ts
+++ Lab4/task1/src/app/app.ts
@@ -2,14 +2,16 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    @if (isServerRunning) {
-      <span>Yes, the server is running</span>
-    } @else {
-      <span>No, the server is not running</span>
-    }
-  `,
+  template: `@for (user of users; track user.id) {
+    <p>{{ user.name }}</p>
+  }`,
 })
 export class App {
-  isServerRunning = true;
+  users = [
+    {id: 0, name: 'Sarah'},
+    {id: 1, name: 'Amy'},
+    {id: 2, name: 'Rachel'},
+    {id: 3, name: 'Jessica'},
+    {id: 4, name: 'Poornima'},
+  ];
 }
